aad9a8df10b3779ca91d11a0620c0d857967a8ba
Jennifer_Light <112646399+jlight30@users.noreply.github.com>
2023-02-10 12:04:29 -0500

    drop down for difficulty

--- app/src/main/java/com/example/froggerhome/initial_config.java
+++ app/src/main/java/com/example/froggerhome/initial_config.java
@@ -2,13 +2,45 @@ package com.example.froggerhome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class initial_config extends AppCompatActivity {
+
+public class initial_config extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_config);
+
+        Spinner spinnerDifficulties=findViewById(R.id.difficulty_dropdown); //instantiate our spinner
+
+
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.difficulty, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDifficulties.setAdapter(adapter);
+        spinnerDifficulties.setOnItemSelectedListener(this);
+
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
