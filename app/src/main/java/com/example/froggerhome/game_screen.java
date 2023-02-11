34839e6092f93fe1465328ed678b1b4a0c16fc89
tlin376 <tlin376@gatech.edu>
2023-02-11 14:43:59 -0500

    game screen changes

--- app/src/main/java/com/example/froggerhome/game_screen.java
+++ app/src/main/java/com/example/froggerhome/game_screen.java
@@ -1,4 +1,15 @@
 package com.example.froggerhome;
 
-public class game_screen {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class game_screen extends AppCompatActivity {
+    private int score = 0;
+    private int lives = 3;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_screen);
+    }
+
 }
--- app/src/main/java/com/example/froggerhome/initial_config.java
+++ app/src/main/java/com/example/froggerhome/initial_config.java
@@ -3,6 +3,7 @@ package com.example.froggerhome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
@@ -12,10 +13,15 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.widget.Button;
 
 public class initial_config extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    private String player_name;
+    private String difficulty;
+    private Button char_1;
+    private Button char_2;
+    private Button char_3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +30,41 @@ public class initial_config extends AppCompatActivity implements AdapterView.OnI
 
         Spinner spinnerDifficulties=findViewById(R.id.difficulty_dropdown); //instantiate our spinner
 
-
         ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.difficulty, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerDifficulties.setAdapter(adapter);
         spinnerDifficulties.setOnItemSelectedListener(this);
+        difficulty = spinnerDifficulties.getSelectedItem().toString();
 
+        char_1 = (Button)findViewById(R.id.char_1);
+        char_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGame();
+            }
+        });
+
+        char_2 = (Button)findViewById(R.id.char_2);
+        char_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGame();
+            }
+        });
+
+        char_3 = (Button)findViewById(R.id.char_3);
+        char_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGame();
+            }
+        });
     }
 
+    public void openGame() {
+        Intent intent = new Intent(this, game_screen.class);
+        startActivity(intent);
+    }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
