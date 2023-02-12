be991973a9190d59d21695c69f8968311d942d23
tlin376 <tlin376@gatech.edu>
2023-02-12 12:25:16 -0500

    started user name box and added frog pngs

--- app/src/main/java/com/example/froggerhome/game_screen.java
+++ app/src/main/java/com/example/froggerhome/game_screen.java
@@ -3,6 +3,8 @@ package com.example.froggerhome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 public class game_screen extends AppCompatActivity {
     private int score = 0;
     private int lives = 3;
@@ -10,6 +12,13 @@ public class game_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+        TextView difficulty_level = (TextView) findViewById(R.id.difficulty_level);
+        if (true) {
+            difficulty_level.setText("Hard");
+        } else if (true) {
+            difficulty_level.setText("Medium");
+        } else {
+            difficulty_level.setText("Easy");
+        }
     }
-
 }
