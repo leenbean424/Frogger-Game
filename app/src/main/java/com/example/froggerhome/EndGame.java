d7026a51473c32b5bca026b0fbff158be05fc3bf
tlin376 <tlin376@gatech.edu>
2023-04-01 16:47:56 -0400

    properly exits game

--- app/src/main/java/com/example/froggerhome/EndGame.java
+++ app/src/main/java/com/example/froggerhome/EndGame.java
@@ -1,8 +1,8 @@
 package com.example.froggerhome;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,27 +10,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class EndGame extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_end_game);
         super.onCreate(savedInstanceState);
 
-        TextView score = (TextView) findViewById(R.id.score);
+        TextView score = findViewById(R.id.score);
         score.setText("Score");
-        TextView finalScore = (TextView) findViewById(R.id.endScore);
+        TextView finalScore = findViewById(R.id.endScore);
         finalScore.setText(Integer.toString(GameScreen.getFinalScore()));
 
-        Button restartGame = (Button) findViewById(R.id.restartGameButton);
-        Button exitGame = (Button) findViewById(R.id.exitGameButton);
+        Button restartGame = findViewById(R.id.restartGameButton);
+        Button exitGame = findViewById(R.id.exitGameButton);
 
-        restartGame.setOnClickListener(v -> {
-            restart();
-        });
+        restartGame.setOnClickListener(v -> restart());
 
-        exitGame.setOnClickListener(v -> finish());
+        exitGame.setOnClickListener(v -> end());
     }
     public void restart() {
         Intent intent = new Intent(this, InitialConfig.class);
         startActivity(intent);
+    }
+
+    public void end() {
+        finishAffinity();
     }
 }
